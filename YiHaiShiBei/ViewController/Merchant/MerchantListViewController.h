7b954e742933ca5a03ce8ce7be1a498e3f2454c5
perrychen901105 <chenzhipeng901105@gmail.com>
2014-12-09 08:03:55 +0800

    add merchant list view

---
+++ YiHaiShiBei/ViewController/Merchant/MerchantListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MerchantListViewController.h
+//  YiHaiShiBei
+//
+//  Created by chenzhipeng on 14/12/9.
+//  Copyright (c) 2014年 perry. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MerchantListViewController : BaseViewController
+
+@end
